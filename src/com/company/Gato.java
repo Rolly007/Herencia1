8c9b6ae522a0c3e5a6ac39012301afeaa817af7e
Americo <r.americo.quiroz@gmail.com>
2018-02-02 21:07:02 -0400

    Conceptos Basicos de Herencia

---
+++ src/com/company/Gato.java
@@ -0,0 +1,14 @@
+package com.company;
+
+public class Gato extends AnimalDomestico {
+
+    public Gato(String nombre, String color){
+        super(nombre, color);
+    }
+
+
+    public void saludar(){
+        System.out.println("miau");
+        //super.saludar();
+    }
+}
---
+++ src/com/company/Main.java
@@ -0,0 +1,11 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	    Gato cat = new Gato("Tom", "Negro");
+	    cat.saludar();
+	    Perro dog= new Perro("Boby", "Cafe","chapi");
+	    dog.saludar();
+    }
+}
